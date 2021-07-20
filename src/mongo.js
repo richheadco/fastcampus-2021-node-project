5ff45901e486b4b01fc823b80bf4bd513b7f7ad2
richheadco <richheadco@users.noreply.github.com>
2021-07-20 12:02:25 +0900

    edit

--- src/mongo.js
+++ src/mongo.js
@@ -2,24 +2,54 @@
 
 const { MongoClient } = require('mongodb')
 
-const { MONGO_PASSWORD, MONGO_CLUSTER, MONGO_USER, MONGO_DBNAME } = process.env
-
-const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_CLUSTER}/${MONGO_DBNAME}?retryWrites=true&w=majority`
+const uri =
+    'mongodb+srv://kevinyoo:Iron1109!@cluster0.s8kvo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
 
-let didConnect = false
-
-async function getUsersCollection() {
-  if (!didConnect) {
+async function main() {
     await client.connect()
-    didConnect = true
-  }
-  return client.db().collection('users')
+
+    const users = client.db('fc21').collection('users')
+
+    // Reset
+    await users.deleteMany({})
+    await users.insertMany([{
+            name: 'John',
+            birthYear: 2000,
+        },
+        {
+            name: 'Smith',
+            birthYear: 1995,
+        },
+        {
+            name: 'Kevin',
+            birthYear: 1990,
+        },
+        {
+            name: 'Ann',
+            birthYear: 1993,
+        },
+    ])
+
+    await users.deleteOne({
+        name: 'John',
+    })
+
+    const courser = users.find({
+        birthYear: {
+            $gte: 1990,
+        },
+    }, {
+        sort: {
+            birthYear: 1,
+        },
+    })
+    await courser.forEach(console.log)
+
+    await client.close()
 }
 
-module.exports = {
-  getUsersCollection,
-}
+main()
